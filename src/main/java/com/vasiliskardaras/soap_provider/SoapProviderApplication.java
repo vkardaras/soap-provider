2018b92ff38ee43c91b7cd5b9d520f7b90c41fcc
vkardaras <vasiliskardaras@gmail.com>
2025-08-07 13:35:05 +0300

    Basic functionality for provider

---
+++ src/main/java/com/vasiliskardaras/soap_provider/SoapProviderApplication.java
@@ -0,0 +1,13 @@
+package com.vasiliskardaras.soap_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapProviderApplication.class, args);
+	}
+
+}
